1d45e4f11bc1f6f1d32e5f92324e05834f2870af
Dan Barthaux <133030848+dBarthaux@users.noreply.github.com>
2026-02-22 10:13:18 -0500

    Refactor ModelParameters module to use alias for wp

--- PARCELY_V2_Fortran/PARCELY_V2/model_parameters.f90
+++ PARCELY_V2_Fortran/PARCELY_V2/model_parameters.f90
@@ -1,12 +1,12 @@
 module ModelParameters
     
-use dvode_kinds_module, only: dvode_wp
+use dvode_kinds_module, only: wp => dvode_wp
     
 implicit none
 
-logical :: INCLUDE_ORGANICS, INCLUDE_COCONDENSE
-real(dvode_wp) :: W, ac, at, sftc, TmpCurrent
+logical :: INCLUDE_ORGANICS, INCLUDE_COCONDENSE, INCLUDE_BAT
+real(wp) :: W, ac, at, sftc, TmpCurrent
 integer ndrop, ninorg, norg, Msft, Cubes
-real(dvode_wp), allocatable :: InorganicBase(:,:), OrganicProperties(:,:), SoluteProperties(:,:), DropSurfTens(:)
+real(wp), allocatable :: InorganicBase(:,:), OrganicProperties(:,:), SoluteProperties(:,:), DropSurfTens(:)
 
 end module ModelParameters
